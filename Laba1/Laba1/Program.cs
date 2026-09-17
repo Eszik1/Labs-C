0d605558002414f51e70e3d7c77190e92dd6d76e
Eszik <vnbrvk@gmail.com>
2026-09-17 15:23:14 +0300

    Добавлена структура и описан метод main

--- Laba1/Laba1/Program.cs
+++ Laba1/Laba1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+struct GeneticData
+{
+    public string protein;
+    public string organism;
+    public string amino_acids;
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Запуск программы генетического поиска...");
+
+
+        List<GeneticData> database = ReadSequences("sequences.txt");
+        Console.WriteLine($"Успешно загружено белков: {database.Count}");
+
+
+        ProcessCommandsAndWriteReport(database, "commands.txt", "genedata.txt");
+        Console.WriteLine("Готово! Результаты сохранены в genedata.txt");
+    }
+}
